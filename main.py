69de6e4c965e33063b7a41912f8d38d505016db9
jisoo lee <jisoo-2664@naver.com>
2026-05-31 18:27:36 +0900

    과제02

--- main.py
+++ main.py
@@ -13,8 +13,7 @@ app.include_router(course_router)
 async def welcome() -> dict:
     return {
         "msg": "Welcome to Hello World API!"
-        }   
-app.include_router(course_router)
+        }
 
 if __name__ =='__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
